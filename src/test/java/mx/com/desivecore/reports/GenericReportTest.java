8e3e6b773fd0285415718196d22bd28b05b354f0
Daniel Demetrio Casas <daniel-casascalderon@hotmail.com>
2024-06-27 13:36:43 -0600

    [Corrección Reportes] Se mitiga error en exportación a Excel en detección de tipo de celdas

--- src/test/java/mx/com/desivecore/reports/GenericReportTest.java
+++ src/test/java/mx/com/desivecore/reports/GenericReportTest.java
@@ -1,11 +1,62 @@
 package mx.com.desivecore.reports;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.java.Log;
+import mx.com.desivecore.commons.models.ResponseModel;
+import mx.com.desivecore.domain.branches.models.Branch;
+import mx.com.desivecore.domain.reports.models.search.InventoryParamsReport;
+import mx.com.desivecore.domain.reports.ports.ReportServicePort;
+
+@Log
 @SpringBootTest
 public class GenericReportTest {
-	
-	
-	
+
+	@Autowired
+	private ReportServicePort reportServicePort;
+
+	@Test
+	@DisplayName("VALID INVENTORY REPORT")
+	public void validInventoryReport() {
+		log.info("INIT validInventoryReport()");
+
+		InventoryParamsReport inventoryParamsReport = new InventoryParamsReport();
+		Branch branch = new Branch();
+		branch.setBranchId(0L);
+		inventoryParamsReport.setBarnch(branch);
+		inventoryParamsReport.setFormat("XSL");
+		
+		ResponseModel responseReport = reportServicePort.generateInventoryReport(inventoryParamsReport);
+		
+		assertAll(
+
+				() -> assertNotEquals(null, responseReport),
+
+				() -> assertNotEquals(null, responseReport.getData()));
+
+		try {
+			String date = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss").format(new Date());
+			String format = inventoryParamsReport.getFormat().equals("PDF") ? ".pdf"
+					: ".xls";
+			FileUtils.writeByteArrayToFile(
+					new File("C:\\Users\\Odalis\\Documents\\reports\\inventory_report" + date + format),
+					(byte[]) responseReport.getData());
+		} catch (IOException e) {
+			log.severe(e.getMessage());
+		}
+
+	}
 
 }
